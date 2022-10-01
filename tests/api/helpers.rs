b928e43317a48ea7a1d7d6dcba7210e5d0ca98fc
vladimir popov <rowdyme0@gmail.com>
2022-10-02 00:08:15 +0300

    add missing column

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -124,11 +124,12 @@ pub async fn spawn_app(gen_params: bool) -> Result<TestApp, std::io::Error> {
 
     let finalized: DB = Data::new(Mutex::new(MerkleTree::new_test(POOL_PARAMS.clone())));
 
-    let jobs = Data::new(kvdb_memorydb::create(3));
+    let jobs = Data::new(kvdb_memorydb::create(4));
     /*
     0 - jobs
-    1 - nullifiers
-    2 - tx to check receipt ( can't query jobs by status )
+    1 - jobs index
+    2 - nullifiers
+    3 - tx to check receipt ( can't query jobs by status )
      */
 
     let vk_str = std::fs::read_to_string(&config.application.tx.vk).unwrap();
