75efc1279d3a229ec7d56b9a45a5ffa26de01c5b
Alexander Filippov <aleksander.fill@gmail.com>
2022-11-21 13:46:55 +0300

    Modify Dockerfile

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-cargo build --release
-cp ./target/release/relayer-rs ./docker
-
-cargo install bunyan
-cp $(which bunyan) ./docker
-
 DOCKER_TAG=$(date +%s)
-docker build --tag zkbob-cloud:$DOCKER_TAG ./docker
+docker build --file ./docker/Dockerfile --tag zkbob-cloud:$DOCKER_TAG .
 docker tag zkbob-cloud:$DOCKER_TAG zkbob-cloud:latest
-
-rm ./docker/relayer-rs
-rm ./docker/bunyan
